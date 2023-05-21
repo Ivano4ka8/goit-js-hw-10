1f90fab06cdfcaa62593bd513b4d5b30fa622742
Ivano4ka8 <ivannamihalova@gmail.com>
2023-05-21 17:23:39 +0300

    add hometask

---
+++ src/js/index.js
@@ -0,0 +1,85 @@
+import { fetchCountries } from './fetschCountries';
+
+import '../css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+const DEBOUNCE_DELAY = 300;
+const mainTitleEl = document.querySelector('.main-title');
+const inputEl = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
+
+mainTitleEl.textContent = 'Please enter name of country';
+
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput() {
+  const countryName = inputEl.value;
+
+  if (countryName === '') {
+    listEl.innerHTML = '';
+    infoEl.innerHTML = '';
+    return;
+  }
+
+  if (!countryName.trim()) {
+    listEl.innerHTML = '';
+    infoEl.innerHTML = '';
+    Notiflix.Notify.info('Рядок не повинен бути пустим');
+    return;
+  }
+
+  fetchCountries(countryName.trim())
+    .then(onSuccess)
+    .catch(error => {
+      Notiflix.Notify.warning('Oops, there is no country with that name');
+    });
+}
+
+function onSuccess(arr) {
+  if (arr.length > 10) {
+    Notiflix.Notify.failure(
+      'Too many matches found. Please enter a more specific name'
+    );
+    listEl.innerHTML = '';
+    infoEl.innerHTML = '';
+    return;
+  }
+
+  if (arr.length === 1) {
+    listEl.innerHTML = '';
+    infoEl.innerHTML = '';
+    createMarkupInfo(arr);
+    return;
+  }
+  createMarkupList(arr);
+}
+
+function createMarkupList(countries) {
+  const markup = countries
+    .map(({ flags, name }) => {
+      return `<li class = "country-item">
+      <img class = "flag-img" src =${flags?.svg} alt =${flags?.alt}>
+    <p class = "name-in-list">${name?.official}</p>
+    </li>`;
+    })
+    .join('');
+  infoEl.innerHTML = '';
+  listEl.innerHTML = markup;
+}
+
+function createMarkupInfo(countries) {
+  const markup = countries
+    .map(({ flags, name, capital, population, languages }) => {
+      return `<div class = "main-wrapper"><img class = "flag-img" src = ${flags?.svg}> 
+      <h2 class = "info-country-name">${name?.official}</h2>
+      </div>
+      <p class = "info-text"><b class = "info-title">Capital:</b>${capital}</p>
+      <p class = "info-text"><b class = "info-title">Population:</b>${population}</p>
+      <p class = "info-text"><b class = "info-title">Languages:</b>${Object.values(languages)}</p>`;
+    })
+    .join('');
+
+  infoEl.innerHTML = markup;
+}
